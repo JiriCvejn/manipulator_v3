ba102b0207bb237f26e435e261558f42cf250ae6
JiriCvejn <jiri.cvejn94@gmail.com>
2025-09-02 21:47:56 +0200

    Update Database.js

--- apps/backend/src/models/Database.js
+++ apps/backend/src/models/Database.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres',
+  process.env.DATABASE_URL || psql 'postgresql://neondb_owner:npg_ZzmQt4fs7DEp@ep-ancient-block-a9vwo2ii-pooler.gwc.azure.neon.tech/neondb?sslmode=require&channel_binding=require',
   {
     logging: false,
     define: {
@@ -13,3 +13,4 @@ export const sequelize = new Sequelize(
     },
   }
 );
+
